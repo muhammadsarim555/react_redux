490689f4496d86fd91f33fd632cd489c1f0fea35
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-10-15 20:18:33 -0700

    added redus work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import CustomRoutes from './Routes';
-import {Provider} from 'react-redux';
-
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {/* <CustomRoutes/> */}
       <h1> I am home from app.js </h1>
-      {/* <Link to="/"> home </Link> */}
 
       </div>
     );
--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import {  Router, Route, Link } from 'react-router-dom';
-import App from './App';
-import Demo from './demo';
-import history from './History';
+// import React, { Component } from 'react';
+// import {  Router, Route, Link } from 'react-router-dom';
+// import App from './App';
+// import Demo from './demo';
+// import history from './History';
 
 
-const CustomRoutes = () => (
-    <Router history={history}>
-        <div>
+// const CustomRoutes = () => (
+//     <Router history={history}>
+//         <div>
 
-            <Link  to="/"> App  </Link>
-            <Link to="/demo"> Demo  </Link>
+//             <Link  to="/"> App  </Link>
+//             <Link to="/demo"> Demo  </Link>
 
-            <Route exact path="/" component={App} />
-            <Route path="/demo" component={Demo} />
-        </div>
-    </Router>
-)
+//             <Route exact path="/" component={App} />
+//             <Route path="/demo" component={Demo} />
+//         </div>
+//     </Router>
+// )
 
-export default CustomRoutes;
+// export default CustomRoutes;
